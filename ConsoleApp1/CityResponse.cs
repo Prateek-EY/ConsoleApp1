32bce3a744d427bb2abcb40269f5e759d87f3066
Prateek_Dev <prateek.dev1@dellteam.com>
2023-02-15 11:30:54 +0530

    Add project files.

---
+++ ConsoleApp1/CityResponse.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+
+namespace ConsoleApp1
+{
+    internal class CityResponse
+    {
+        [JsonProperty("latitude")]
+        public string latitude { get; set; }
+        [JsonProperty("longitude")]
+        public string longitude { get; set; }
+
+        [JsonProperty("current_weather")]
+        public CurrentWeather _currentWeather { get; set; }
+
+
+    }
+
+    public class CurrentWeather
+    {
+        [JsonProperty("temperature")]
+        public string temperature{ get; set; }
+        [JsonProperty("windspeed")]
+        public string windspeed { get; set; }
+        [JsonProperty("winddirection")]
+        public string winddirection { get; set; }
+        [JsonProperty("weathercode")]
+        public string weathercode { get; set; }
+
+        [JsonProperty("time")]
+        public DateTime dateTime { get; set; }
+
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Data;
+using System.Web;
+using ConsoleApp1;
+using IronXL;
+using Newtonsoft.Json;
+
+string cityName = Console.ReadLine();
+WorkBook workBook = WorkBook.Load(@"C:\Users\Prateek_Dev\OneDrive - Dell Technologies\Desktop\City\Cities.xlsx");
+WorkSheet workSheet = workBook.DefaultWorkSheet;
+
+// Convert the worksheet to DataTable
+DataTable dataTable = workSheet.ToDataTable(true);
+
+// Enumerate by rows or columns first at your preference
+var result_rows = dataTable.AsEnumerable().Where(r => r.Field<string>("city_ascii") == cityName).ToArray();
+var lat = result_rows[0].ItemArray[2];
+var longitude = result_rows[0].ItemArray[3];
+using (var httpClient = new HttpClient())
+{
+    string requestUri = "https://api.open-meteo.com/v1/forecast";
+    var uriBuilder = new UriBuilder(requestUri);
+    var paramValues = HttpUtility.ParseQueryString(uriBuilder.Query);
+    paramValues.Add("latitude", (string?)lat.ToString());
+    paramValues.Add("longitude", (string?)longitude.ToString());
+    paramValues.Add("current_weather","true");
+    uriBuilder.Query = paramValues.ToString();
+    using (var response = await httpClient.GetAsync(uriBuilder.Uri))
+    {
+        string apiResponse = await response.Content.ReadAsStringAsync();
+        var response_json = JsonConvert.DeserializeObject<CityResponse>(apiResponse);
+    }
+
+}
